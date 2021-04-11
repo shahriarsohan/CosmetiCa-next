17a50d0f2119a0f18d4d464ab34b1e91e90f068d
shahriarsohan <sohanmock@gmail.com>
2021-04-11 21:32:07 +0600

    some seo tags updated

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    swSrc: "service-worker.js",
-  },
+module.exports = {
   images: {
     domains: ["romex.s3.ap-south-1.amazonaws.com", "cosmetica.com.bd"],
   },
   future: { webpack5: true },
-});
+};
